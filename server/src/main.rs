711dd79e600a10ba236310566e15bd26fb165ef7
novacrazy <novacrazy@gmail.com>
2020-06-06 04:45:12 -0500

    Add simple server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use warp::{Filter, Rejection, Reply};
+
+pub fn files() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
+    warp::fs::dir("../client/dist")
+}
+
+pub fn index() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
+    warp::fs::file("../client/dist/index.html")
+}
+
+#[tokio::main]
+async fn main() {
+    warp::serve(files().or(index())).run(([127, 0, 0, 1], 9009)).await;
 }
